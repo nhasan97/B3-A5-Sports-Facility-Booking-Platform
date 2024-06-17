b6425e4bcb49f241555b2c8401695b7496ae8204
nhasan97 <nh6069325@gmail.com>
2024-06-17 21:04:33 +0600

    used doesFacilityExist static function in updateFacility and deleteFacility controller functions for checking the existance of facility before update or delete operation in facility.controllers.ts

--- src/app/modules/facility/facility.controllers.ts
+++ src/app/modules/facility/facility.controllers.ts
@@ -2,6 +2,8 @@ import httpStatus from 'http-status';
 import { sendResponse } from '../../utilities/sendResponse';
 import { facilityServices } from './facility.services';
 import catchAsync from '../../utilities/catchAsync';
+import { facilityModel } from './facility.model';
+import AppError from '../../errors/AppError';
 /*
 
 ----------------controller for inserting new facility data in DB----------------*/
@@ -19,10 +21,19 @@ const createFacility = catchAsync(async (req, res) => {
 
 --------------controller for updating specific facility info in DB----------------*/
 const updateFacility = catchAsync(async (req, res) => {
+  //checking if the selected facility exists or not. If not throwing an error.
+  const loadedFacility = await facilityModel.doesFacilityExist(req.params.id);
+  if (!loadedFacility) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Facility not found');
+  }
+
+  //Passing data to service function
   const response = await facilityServices.updateFacilityIntoDB(
     req.params.id,
     req.body,
   );
+
+  //sending response
   sendResponse(
     res,
     httpStatus.OK,
@@ -35,7 +46,16 @@ const updateFacility = catchAsync(async (req, res) => {
 
 --------------controller for updating specific facility info in DB----------------*/
 const deleteFacility = catchAsync(async (req, res) => {
+  //checking if the selected facility exists or not. If not throwing an error.
+  const loadedFacility = await facilityModel.doesFacilityExist(req.params.id);
+  if (!loadedFacility) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Facility not found');
+  }
+
+  //deleting facility from db
   const response = await facilityServices.deleteFacilityFromDB(req.params.id);
+
+  //sending response
   sendResponse(
     res,
     httpStatus.OK,
